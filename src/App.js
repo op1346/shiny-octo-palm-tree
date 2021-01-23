4da8355c1ac4b7eb42f9f8b08debacb7f4369779
op1346 <olivia.park1346@gmail.com>
2021-01-23 16:03:01 +0900

    Add file uploader to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import FileUploader from './components/FileUploader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="container" style={{ width: "600px" }}>
+      <div className="my-3">
+        Resume Upload
+      </div>
+
+      <FileUploader />
     </div>
   );
 }
